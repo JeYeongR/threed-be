b8112e0b106f59935af9603ec83042aa182f82b2
JeYeongR <wpdud2003@gmail.com>
2025-07-09 23:11:06 +0900

    chore: CI/CD 방식 변경

--- scripts/start-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "--------------- 서버 배포 시작 -----------------"
-docker stop threed || true
-docker rm threed || true
-docker pull 113790821400.dkr.ecr.ap-northeast-2.amazonaws.com/threed:latest
-docker run -d --name threed -p 80:8080 113790821400.dkr.ecr.ap-northeast-2.amazonaws.com/threed:latest
-echo "--------------- 서버 배포 끝 -----------------"
